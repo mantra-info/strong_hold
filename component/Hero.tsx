cd377ff0ee40d6f7696c1b75bf20ac51fed9ea23
mantra-info <albie@mantraitsolutions.in>
2026-04-23 17:33:06 +0530

    update

--- component/Hero.tsx
+++ component/Hero.tsx
@@ -11,6 +11,7 @@ const Hero = () => {
                 muted
                 loop
                 playsInline
+                poster="/SH-Img1.webp"
                 className="absolute top-0 left-0 w-full h-full object-cover z-0"
             >
                 <source src="/hero-video.mp4" type="video/mp4" />
